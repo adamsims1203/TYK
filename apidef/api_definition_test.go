adamsims1203/TYK

commit 24947355ee0c79ce84dfb47026475a8dbc281893

ignore ValidatePathMeta.Schema from datastore (#4099)
`ValidatePathMeta.Schema64` will be considered over `ValidatePathMeta.Schema`

## Related Issue
https://tyktech.atlassian.net/browse/TT-5547

## Types of changes
<!-- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
- [x] Bug fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing functionality to change)
- [ ] Refactoring or add test (improvements in base code or adds test coverage to functionality)

apidef/api_definitions.go
@@ -323,8 +323,8 @@ type ValidatePathMeta struct {
 	Disabled    bool                    `bson:"disabled" json:"disabled"`
 	Path        string                  `bson:"path" json:"path"`
 	Method      string                  `bson:"method" json:"method"`
-	Schema      map[string]interface{}  `bson:"schema" json:"schema"`
-	SchemaB64   string                  `bson:"schema_b64" json:"schema_b64,omitempty"`
+	Schema      map[string]interface{}  `bson:"-" json:"schema"`
+	SchemaB64   string                  `bson:"schema_b64" json:"-"`
 	SchemaCache gojsonschema.JSONLoader `bson:"-" json:"-"`
 	// Allows override of default 422 Unprocessible Entity response code for validation errors.
 	ErrorResponseCode int `bson:"error_response_code" json:"error_response_code"`

commit 33c7a9507c484fe93d4704192d41594e70edc5e6

[TT-7362] Fix validate JSON middleware not loading in worker gateway. (#4719)
https://tyktech.atlassian.net/browse/TT-7362

caused by:  https://github.com/TykTechnologies/tyk/pull/4099

apidef/api_definitions.go
@@ -340,7 +340,7 @@ type ValidatePathMeta struct {
 	Path        string                  `bson:"path" json:"path"`
 	Method      string                  `bson:"method" json:"method"`
 	Schema      map[string]interface{}  `bson:"-" json:"schema"`
-	SchemaB64   string                  `bson:"schema_b64" json:"-"`
+	SchemaB64   string                  `bson:"schema_b64" json:"schema_b64,omitempty"`
 	SchemaCache gojsonschema.JSONLoader `bson:"-" json:"-"`
 	// Allows override of default 422 Unprocessible Entity response code for validation errors.
 	ErrorResponseCode int `bson:"error_response_code" json:"error_response_code"`

apidef/api_definition_test.go
@@ -1,6 +1,9 @@
 package apidef
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -102,6 +105,36 @@ func TestDecodeFromDB(t *testing.T) {
 			ScopeToPolicy:  scopeToPolicyMap,
 		}
 		assert.Equal(t, expectedOICScope, spec.Scopes.OIDC)
+	})
+
+	t.Run("json schema validation middleware", func(t *testing.T) {
+		apiDef := DummyAPI()
+		var (
+			bodySchema map[string]interface{}
+			v1         = "v1"
+			v1B64      = base64.StdEncoding.EncodeToString([]byte(v1))
+		)
+		err := json.Unmarshal([]byte(`{"$schema":"http://json-schema.org/draft-04/schema#","properties":{"id":{"type":"integer"}},"required":["id"],"type":"object"}`),
+			&bodySchema)
+		assert.NoError(t, err)
+		apiDef.VersionData.Versions[v1] = VersionInfo{
+			ExtendedPaths: ExtendedPathsSet{
+				ValidateJSON: []ValidatePathMeta{
+					{
+						Path:   "/",
+						Method: http.MethodPost,
+						Schema: bodySchema,
+					},
+				},
+			},
+		}
+		apiDef.EncodeForDB()
+		copyAPIDef := apiDef
+		copyAPIDef.DecodeFromDB()
+
+		assert.Equal(t, apiDef.VersionData.Versions[v1B64].ExtendedPaths.ValidateJSON[0].Schema,
+			copyAPIDef.VersionData.Versions[v1].ExtendedPaths.ValidateJSON[0].Schema)
+		assert.Empty(t, copyAPIDef.VersionData.Versions[v1].ExtendedPaths.ValidateJSON[0].SchemaB64)
 	})
 }
 